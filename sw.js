6986133a0a991fb2c9b2d71dbc1a702ef5376149
robo-mop <realaryanpingle@gmail.com>
2022-06-15 10:37:36 +0530

    Added standalone app version

--- sw.js
+++ sw.js
@@ -5,6 +5,8 @@
 //       Suck it WorkboxJS.      //
 ///////////////////////////////////
 
+const APP_VERSION = 1.30
+
 // Document Cache is a cache of document files - html, js, css, etc
 const DOCUMENT_CACHE_NAME = `DOC`
 var DOCUMENT_CACHE = null
